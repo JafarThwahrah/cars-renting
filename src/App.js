1c4c7132b1d31bbebb1ad64b10dfee2a46954381
JafarThwahrah <jaffardawahreh2@gmail.com>
2022-11-20 22:49:42 +0200

    push Notification

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import Pages from "./routes/Pages";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import './App.css';
+import "./App.css";
+import { Notifications } from "react-push-notification";
+
 function App() {
   return (
     <div className="App">
+      <Notifications />
       <BrowserRouter>
         <Header />
         <Pages />
